48c17701da5cd3aaf03b3ba9d0b9a65b58780de3
Alexis Jacomy <alexis.jacomy@gmail.com>
2014-09-12 13:56:48 +0200

    Improved version for browser
    Added the MIT License at the beginning of the file.
    Encapsulated the code in a self-calling function (to prevent scope pollution).
    Export in global if nor module, exports  neither module.exports are present.

--- index.js
+++ index.js
@@ -1,3 +1,33 @@
+/**
+ * typology.js - a type checking library for Node.js and the browser,
+ *
+ * Version: 0.0.1
+ * Sources: http://github.com/jacomyal/typology
+ * Doc:     http://github.com/jacomyal/typology#readme
+ *
+ * License:
+ * --------
+ * Copyright © 2014 Alexis Jacomy (@jacomyal), Guillaume Plique (@Yomguithereal)
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to
+ * deal in the Software without restriction, including without limitation the
+ * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+ * sell copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * The Software is provided "as is", without warranty of any kind, express or
+ * implied, including but not limited to the warranties of merchantability,
+ * fitness for a particular purpose and noninfringement. In no event shall the
+ * authors or copyright holders be liable for any claim, damages or other
+ * liability, whether in an action of contract, tort or otherwise, arising
+ * from, out of or in connection with the software or the use or other dealings
+ * in the Software.
+ */
+;(function(global) {
 'use strict';
 
 var k,
@@ -185,4 +215,10 @@ types.add('type', function(v) {
   return types.isValid(v);
 });
 
-module.exports = types;
+if (typeof exports !== 'undefined') {
+  if (typeof module !== 'undefined' && module.exports)
+    exports = module.exports = types;
+  exports.types = types;
+}
+global.types = types;
+})(this);
